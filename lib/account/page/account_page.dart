1a8aff314f2f1e8a59f422a9618f6e47bb47ce0e
Nana Kwame <aduhenry9@gmail.com>
2024-02-01 22:45:17 +0000

    feat: Add file_selector_windows plugin and update dependencies

--- lib/account/page/account_page.dart
+++ lib/account/page/account_page.dart
@@ -1,5 +1,6 @@
 import "package:flutter/material.dart";
 import "package:supabase_flutter/supabase_flutter.dart";
+import "package:supabase_quickstart/account/components/avatar.dart";
 import "package:supabase_quickstart/bootstrap.dart";
 
 class AccountPage extends StatefulWidget {
@@ -13,6 +14,7 @@ class _AccountPageState extends State<AccountPage> {
   final _usernameController = TextEditingController();
   final _websiteController = TextEditingController();
 
+  String? _avatarUrl;
   var _loading = true;
 
   /// Called once a user id is received within `onAuthenticated()`
@@ -22,11 +24,12 @@ class _AccountPageState extends State<AccountPage> {
     });
 
     try {
-      final userId = supabase.auth.currentUser!.id;
+      final userId = supabase.auth.currentSession!.user.id;
       final data =
           await supabase.from("profiles").select().eq("id", userId).single();
       _usernameController.text = (data["username"] ?? "") as String;
       _websiteController.text = (data["website"] ?? "") as String;
+      _avatarUrl = (data["avatar_url"] ?? "") as String;
     } on PostgrestException catch (error) {
       SnackBar(
         content: Text(error.message),
@@ -60,7 +63,6 @@ class _AccountPageState extends State<AccountPage> {
       "website": website,
       "updated_at": DateTime.now().toIso8601String(),
     };
-
     try {
       await supabase.from("profiles").upsert(updates);
       if (mounted) {
@@ -107,6 +109,39 @@ class _AccountPageState extends State<AccountPage> {
     }
   }
 
+  /// Called when image has been uploaded to Supabase storage from within Avatar widget
+  Future<void> _onUpload(String imageUrl) async {
+    try {
+      final userId = supabase.auth.currentUser!.id;
+      await supabase.from("profiles").upsert({
+        "id": userId,
+        "avatar_url": imageUrl,
+      });
+      if (mounted) {
+        const SnackBar(
+          content: Text("Updated your profile image!"),
+        );
+      }
+    } on PostgrestException catch (error) {
+      SnackBar(
+        content: Text(error.message),
+        backgroundColor: Theme.of(context).colorScheme.error,
+      );
+    } catch (error) {
+      SnackBar(
+        content: const Text("Unexpected error occurred"),
+        backgroundColor: Theme.of(context).colorScheme.error,
+      );
+    }
+    if (!mounted) {
+      return;
+    }
+
+    setState(() {
+      _avatarUrl = imageUrl;
+    });
+  }
+
   @override
   void initState() {
     super.initState();
@@ -129,6 +164,11 @@ class _AccountPageState extends State<AccountPage> {
           : ListView(
               padding: const EdgeInsets.symmetric(vertical: 18, horizontal: 12),
               children: [
+                Avatar(
+                  imageUrl: _avatarUrl,
+                  onUpload: _onUpload,
+                ),
+                const SizedBox(height: 18),
                 TextFormField(
                   controller: _usernameController,
                   decoration: const InputDecoration(labelText: "User Name"),
